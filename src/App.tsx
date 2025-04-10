49b2f8764c6f4c743906d4f69e56f5e93ce5772e
gautamabhish <abhishekgautam.080470@gmail.com>
2025-04-10 21:32:11 +0530

    Changingf from webtorrent to tree architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 
 import './App.css'
 import { BrowserRouter as Router , Routes ,Route } from 'react-router'
-import ViewUI from './Components/pages/ViewUI'
-import StreamUI from './Components/pages/StreamUI'
+
 
 function App() {
  
@@ -12,9 +11,7 @@ function App() {
     <Router>
       <Routes>
 
-     
-      <Route path="/" element={<ViewUI/>} />
-      <Route path='/stream' element={<StreamUI/>}></Route>
+
 
       </Routes>
   
